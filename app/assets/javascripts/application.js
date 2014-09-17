cd5ab020b7e1e2a01363213e3622166642402dda
Linnet States <lstates@iqt.org>
2014-09-16 23:19:53 -0400

    getting closer to fixing the layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,5 @@
 //= require flatty/jquery/jquery-migrate.min
 //= require flatty/jquery/jquery-ui.min
 //= require flatty/bootstrap/bootstrap.min
-//= require flatty/plugins/plugins
 //= require flatty/theme
 //= require_tree .
