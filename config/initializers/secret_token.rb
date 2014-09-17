3cdc1c4b6d1120ed2dc20d6fffceb1b29e1306be
Linnet States <lstates@iqt.org>
2014-09-16 21:57:36 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularRails::Application.config.secret_key_base = '1efa5b86ceb790f45f4912aa61c6f66966290ddfb6f264f31e0ab231700dac9175e0bd5aa88d1c8d056a5bfc325216af2ab2e8dbe4d19783c8d02adb0ef42117'
